5fb33c2780015db1595edbe3178227a0234c91f2
barrientosvela <jbarrientosvela@gmail.com>
2022-11-04 19:34:41 +0100

    añade funcionalidad

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>No vendo una chapa</title>
+    <title>Document</title>
 </head>
 
 <body>
     <?php
     // Declara la variable $errores para poder mostrar los errores o validar el envio si esta vacia
     $errores = "";
-    $extras = null;
+    $extrasCadena = "";    
     // Obtiene los valores del formulario
     $tipo = $_REQUEST['tipo'];
     $zona = $_REQUEST['zona'];
@@ -20,9 +20,16 @@
     $dormitorios = $_REQUEST['dormitorios'];
     $precio = $_REQUEST['precio'];
     $tamano = $_REQUEST['metros'];
-    copy($_FILES['foto']['tmp_name'],$_FILES['foto']['name']);
-    $foto = $_FILES['foto']['name'];
-    $extras = $_REQUEST['extras'];
+    if (!empty($_FILES['foto']['tmp_name']) && $_FILES['foto']['size'] <= 100000) {
+        copy($_FILES['foto']['tmp_name'],$_FILES['foto']['name']);
+        $foto = $_FILES['foto']['name'];
+        $rutaFoto = "C:/xampp/htdocs/practica/images/".$foto;
+    }
+    if (!empty($_REQUEST['extras'])) {
+        if (is_array($_REQUEST['extras']))
+        $extras = $_REQUEST['extras'];
+        //oreach 
+    }
     $observaciones = $_REQUEST['observaciones'];
 
     // Comprueba valores correctos
@@ -35,6 +42,9 @@
     if (!is_numeric($tamano)) {
         $errores = $errores . "<li>El tamaño debe ser un valor numérico\n</li>";
     }
+    if ($_FILES['foto']['size'] >= 100000) {
+        $errores = $errores . "<li>El tamaño de la foto es mayor de 100kb\n</li>";
+    }
     // Si hay errores los muestra sino muestra los datos introducidos
     if ($errores == "") {
         print("<p>Estos son los datos introducidos:</p>\n");
@@ -44,16 +54,29 @@
         print("   <li>Dirección: $direccion\n");
         print("   <li>Número de dormitorios: $dormitorios\n");
         print("   <li>Precio: $precio &euro;\n");
-        print("   <li>Tamaño: $tamano m<sup>2</sup>\n");        
+        print("   <li>Tamaño: $tamano m<sup>2</sup>\n");
         print("   <li>Extras: ");
-        foreach ($extras as $extra)
-        print($extra . " ");
-
-     print("\n");
+        if (!empty($extras)){
+            foreach ($extras as $extra)
+                print($extra . " ");
+    
+            print("\n");
+        }
         print("   <li>Observaciones: $observaciones\n");
-        print("   <li><img src=\"$foto\">");
+        if (!empty($_FILES['foto']['tmp_name'])) {
+            print("   <li><img src=\"$foto\">");
+        }
         print("</ul>\n");
-        print("<p><a href='principal.html'>Insertar otra vivienda</a></p>");
+        $conect = mysqli_connect("localhost", "root", "", "viviendas");
+        if (mysqli_connect_errno()) {
+            echo "Fallo" . mysqli_connect_error();
+            exit;
+        } else {
+            mysqli_query($conect, "INSERT INTO tabla_viviendas (id, tipo, zona, direccion, dormitorios, precio, tamano, extras, foto, observaciones)
+            VALUES ('','$tipo', '$zona', '$direccion', '$dormitorios','$precio','$tamano','$extras','$rutaFoto','$observaciones')");
+        }
+        print("<p><a href='principal.html'><button>Insertar otra vivienda</button></a></p>");
+        print("<p><a href='consulta.php'><button>Buscar vivienda</button></a></p>");
     } else {
         print("<p>No se ha podido realizar la inserción debido a los siguientes errores:</p>\n");
         print("<ul>");
